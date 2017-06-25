c1b8e5a16e940bc49bc26a40826bc6432929051e
Fernando Silveira <fsilveira@gmail.com>
2017-06-25 13:05:18 -0300

    Improved links in the navigation bar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_to_in_li(body, url, link_to_options = {})
+    active = 'active' if current_page?(url)
+    url = '#' if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url, link_to_options
+    end
+  end
 end
