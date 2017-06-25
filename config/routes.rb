7aa56674acbf567fac0ab0ace7b85433ae238efe
Fernando Silveira <fsilveira@gmail.com>
2017-06-25 01:03:11 -0300

    Added links to the Welcome page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
 
   resources :prospections
   resources :users
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
   resources :activities
   resources :products
   resources :departments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
