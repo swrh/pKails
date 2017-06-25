2824577758035b16ca8a5487833ddcada547270b
Fernando Silveira <fsilveira@gmail.com>
2017-06-25 16:10:25 -0300

    Started adding seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+Department.create([{ name: 'Sales' },
+                   { name: 'Human Resources' },
+                   { name: 'Engineering' },
+                   { name: 'Marketing' },
+                   { name: 'Finance' },
+                   { name: 'Administrative' },
+                   { name: 'Production' }])
+
+Product.create([{ name: 'Word' },
+                { name: 'Excel' },
+                { name: 'PowerPoint' },
+                { name: 'Access' },
+                { name: 'Outlook' },
+                { name: 'Exchange' }])
+
+Region.create([{ name: 'North' },
+               { name: 'South' },
+               { name: 'East' },
+               { name: 'West' }])
+
+Representative.create([{ company: 'Microsoft' },
+                       { company: 'Oracle' },
+                       { company: 'Apple' },
+                       { company: 'Facebook' },
+                       { company: 'Twitter' }])
+
+User.create([{ name: 'John Doe', email: 'john.doe@microsoft.com', department: Department.find_by({ name: 'Marketing' }), representative: Representative.find_by({ company: 'Microsoft' }), password: 'foobar' },
+             { name: 'Jane Doe', email: 'jane.doe@apple.com', department: Department.find_by({ name: 'Sales' }), representative: Representative.find_by({ company: 'Apple' }), password: 'foobar' },
+             { name: 'Joe Bloggs', email: 'joe.bloggs@oracle.com', department: Department.find_by({ name: 'Engineering' }), representative: Representative.find_by({ company: 'Oracle' }), password: 'foobar' }])
+
+ProposalStatus.create([{ name: 'Analysis' },
+                       { name: 'Writing' },
+                       { name: 'Declined', finish: true },
+                       { name: 'Accepted', finish: true }])
+
+Customer.create([{ name: 'Walmart', company: 'Wal-Mart Stores, Inc.', cnpj: '00.000.000/0000-00' },
+                 { name: 'Toys "R" Us', company: 'Toys "R" Us, Inc.', cnpj: '11.111.111/1111-11' },
+                 { name: 'Kmart', company: 'Kmart Corporation', cnpj: '22.222.222/2222-22' }])
